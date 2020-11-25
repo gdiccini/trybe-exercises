893e494ccfb0cb02c6248a6c54188586f856c347
Giovani L D <giovani.diccini@hotmail.com>
2020-11-25 19:19:46 -0300

    exercise 7

---
+++ modulo1/bloco_4/dia_1/ex7.js
@@ -0,0 +1,21 @@
+let p = 55;
+
+switch (true) {
+    case p >= 90:
+        console.log('A');
+        break;
+    case (p >= 80):
+        console.log('B');
+        break;
+    case (p >= 70):
+        console.log('C');
+        break;
+    case (p >= 60):
+        console.log('D');
+        break;
+    case (p >= 50):
+        console.log('E');
+        break;
+    default:
+        console.log('F');
+}
