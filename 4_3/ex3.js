838c3d2947f487ccc2e8b9d3547afb4410b60ee3
Giovani L D <giovani.diccini@hotmail.com>
2020-12-01 11:08:34 -0300

    atualização ex 3

--- 4_3/ex3.js
+++ 4_3/ex3.js
@@ -2,10 +2,11 @@ let n = 5;
 
 for(let i = 0; i < n; i += 1){
     let line = '';
+    
     for(let j = 1 + i; j < n ; j += 1){
         line += ' ';
     }
-        
+
     for (let k = 0; k < i + 1 ; k += 1){
         line += '*'
     }
