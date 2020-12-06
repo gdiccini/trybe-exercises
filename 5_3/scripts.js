5100b2b82eedca6dbc3ed9c245681b4268b1cad2
Giovani L D <giovani.diccini@hotmail.com>
2020-12-06 17:21:14 -0300

    ex10

--- 5_3/scripts.js
+++ 5_3/scripts.js
@@ -148,18 +148,41 @@ function createDaysOfTheWeek() {
 
   coloredSubtitle('blue');
 
-  // ex 9
+  // ex9
   function selectTask(){
-    let taskSelected = document.querySelector('.my-tasks div');
-    taskSelected.addEventListener('click', function(event){
-      if (taskSelected.className = 'task'){
-        taskSelected.className = 'task selected';
+    let selectedTask = document.getElementsByClassName('task selected');
+    let myTask = document.querySelector('.task');
+    
+    myTask.addEventListener('click', function(event){
+      if (selectedTask.length === 0){
+        event.target.className = 'task selected';
       } else {
-        taskSelected.className = 'task';
+        event.target.className = 'task';
       }
-    })
-  }
+    });
+  };
 
   selectTask();
 
+  
+
+  // ex10
+  function setDayColor() {
+    let selectedTask = document.getElementsByClassName('task selected');
+    let days = document.querySelector('#days');
+    let taskDiv = document.querySelector('.task');
+    let taskColor = taskDiv.style.backgroundColor;
+    
+    days.addEventListener('click', function(event){
+      let eventTargetColor = event.target.style.color;
+      if (selectedTask.length > 0 && eventTargetColor !== taskColor) {
+        let color = selectedTask[0].style.backgroundColor;
+        event.target.style.color = color;
+      } else if (eventTargetColor === taskColor && selectedTask.length !== 0) {
+        event.target.style.color = 'rgb(119,119,119)';
+      }
+    });
+  };
+  
+  setDayColor();
   
