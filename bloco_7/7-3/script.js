79085c659a9aae92e0c649db28140296fa9dda06
Giovani L D <giovani.diccini@hotmail.com>
2021-01-15 11:22:41 -0300

    ex2 done

--- bloco_7/7-3/script.js
+++ bloco_7/7-3/script.js
@@ -13,3 +13,24 @@ function sum(a, b) {
 assert.strictEqual(sum(4, 5), 9);
 assert.strictEqual(sum(0, 0), 0);
 assert.throws(() => {sum(4,'5')}, /^Error: parameters must be numbers$/);
+
+// Ex - 2
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+let arr1 = [1, 2, 3, 4];
+myRemove(arr1, 3);
+assert.deepStrictEqual(arr1, [1, 2, 3, 4]);
+
+assert.deepStrictEqual(myRemove(arr1, 5), [1, 2, 3, 4]);
