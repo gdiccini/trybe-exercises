9f81f17ac7095f0221e2e584d2630e6a777e43c1
Giovani Lopes Diccini <giovani-diccini@pop-os.localdomain>
2021-02-03 19:21:37 -0300

    implements new tests

--- bloco10/d1/exAntigos.js
+++ bloco10/d1/exAntigos.js
@@ -8,4 +8,16 @@ function sum(a, b) {
   return a + b;
 }
 
-module.exports = { sum }
+// Ex2
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+module.exports = { sum, myRemove }
--- bloco10/d1/exAntigos.test.js
+++ bloco10/d1/exAntigos.test.js
@@ -1,4 +1,4 @@
-const { sum } = require('./exAntigos');
+const { sum, myRemove } = require('./exAntigos');
 
 describe('sum tests', () => {
   it('Comprova que a soma de 3 + 4 = 7', () => {
@@ -12,4 +12,18 @@ describe('sum tests', () => {
   it('Verifica entrada de strings', () => {
     expect(() => { sum('3', 5) }).toThrowError(new Error('parameters must be numbers'))
   });
+});
+
+describe('myRemove tests', () => {
+  it('my remove recebe como argumento [1, 2, 3, 4] e 3 e retorna [1, 2, 4]', () => {
+    const arr = [1, 2, 3, 4];
+    expect(myRemove(arr, 3)).toEqual([1, 2, 4])
+  })
+
+  it('myremove([1, 2, 3, 4], 3) != [1, 2, 3, 4]', () => {
+    const arr = [1, 2, 3, 4];
+    expect(myRemove(arr, 3)).not.toEqual([1, 2, 3, 4])
+  })
+
+
 });
