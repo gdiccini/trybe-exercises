4ea0840b43dcfb39779d9309f24bc5236bdddf33
Giovani Lopes Diccini <giovani-diccini@pop-os.localdomain>
2021-02-04 21:57:15 -0300

    implements exercise 2 - 3 - 4

---
+++ bloco10/dia2/exercise2-3.test.js
@@ -0,0 +1,29 @@
+const { getUserName, findUserById } = require('./exercise2-3');
+
+// describe('Testes getUserName', () => {
+//   it('Verify if getUserName is a function', () => {
+//     expect(typeof getUserName).toBe('function');
+//   });
+
+//   it('Returns a name who is in the data', done => {
+//     expect(getUserName(4)).resolves.toBe('Mark');
+//     done();
+//   });
+
+//   it('Returns erro when user is not found', done => {
+//     expect(getUserName(10)).rejects.toEqual({ error: 'User with 10 not found.' });
+//     done();
+//   });
+// });
+
+describe('Testes utilizando getUserName async/await', () => {
+  it('Verify user with id exists', async () => {
+    expect.assertions(1);
+    await expect(getUserName(4)).resolves.toBe('Mark');
+  });
+
+  it('Returns erro when user is not found', async () => {
+    expect.assertions(1);
+    await expect(getUserName(10)).rejects.toEqual({ error: 'User with 10 not found.' });
+  })
+});
